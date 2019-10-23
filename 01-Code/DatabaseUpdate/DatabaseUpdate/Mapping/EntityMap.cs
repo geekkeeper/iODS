c8039e118badf66055b9bae9a4f0443da0d647b0
Axun <2722636556@qq.com>
2019-10-23 22:27:47 +0800

    使用迁移创建Department表成功

--- 01-Code/DatabaseUpdate/DatabaseUpdate/Mapping/EntityMap.cs
+++ 01-Code/DatabaseUpdate/DatabaseUpdate/Mapping/EntityMap.cs
@@ -26,35 +26,35 @@ namespace DatabaseUpdate.Mapping
                   .HasName("PK_DepartmentID");                              //主键名称
             entity.Property(d => d.Id)                                      //--部门ID--
                   .HasColumnName(nameof(Department.Id))                     //字段名称
-                  .HasColumnType("int(9)")                                  //字段类型
+                  .HasColumnType("int")                                     //字段类型
                   .HasDefaultValueSql("NEXT VALUE FOR shared.DepartmentId") //设置自增
                   .IsRequired();                                            //不能为空
             entity.Property(d => d.DepartmentName)                          //--部门名称--
                   .HasColumnName(nameof(Department.DepartmentName))         //字段名称
                   .HasColumnType("varchar(20)")                             //字段类型
                   .IsRequired();                                            //不能为空
-            entity.Property(d => d.Branch)
-                  .HasColumnName(nameof(Department.Branch))
-                  .IsRequired();
-            entity.Property(d => d.SuperiorId)
-                  .HasColumnName(nameof(Department.SuperiorId));
-            entity.Property(d => d.Leader)
-                  .HasColumnName(nameof(Department.Leader));
-            entity.Property(d => d.CreatedTime)
-                  .HasColumnName(nameof(Department.CreatedTime))
-                  .IsRequired()
-                  .ValueGeneratedOnAdd();
-            entity.Property(d => d.CreatedBy)
-                  .HasColumnName(nameof(Department.CreatedBy))
-                  .IsRequired();
-            entity.Property(d => d.UpdatedTime)
-                  .HasColumnName(nameof(Department.UpdatedTime));
-            entity.Property(d => d.UpdatedBy)
-                  .HasColumnName(nameof(Department.UpdatedBy))
-                  .ValueGeneratedOnUpdate();
-            entity.Property(d => d.Remark)
-                  .HasColumnName(nameof(Department.Remark));
-            entity.Ignore(d => d.Reserved);
+            entity.Property(d => d.Branch)                                  //-是否是公司-
+                  .HasColumnName(nameof(Department.Branch))                 //字段名称
+                  .IsRequired();                                            //不能为Null
+            entity.Property(d => d.SuperiorId)                              //--上级部门--
+                  .HasColumnName(nameof(Department.SuperiorId));            //字段名称
+            entity.Property(d => d.Leader)                                  //--部门领导--
+                  .HasColumnName(nameof(Department.Leader));                //字段名称
+            entity.Property(d => d.CreatedTime)                             //--创建时间--
+                  .HasColumnName(nameof(Department.CreatedTime))            //字段名称
+                  .IsRequired()                                             //不能为Null
+                  .ValueGeneratedOnAdd();                                   //添加时生成
+            entity.Property(d => d.CreatedBy)                               //---创建人---
+                  .HasColumnName(nameof(Department.CreatedBy))              //字段名称
+                  .IsRequired();                                            //不能为Null
+            entity.Property(d => d.UpdatedTime)                             //--更新时间--
+                  .HasColumnName(nameof(Department.UpdatedTime));           //字段名称
+            entity.Property(d => d.UpdatedBy)                               //---更新人---
+                  .HasColumnName(nameof(Department.UpdatedBy))              //字段名称
+                  .ValueGeneratedOnUpdate();                                //更新时生产
+            entity.Property(d => d.Remark)                                  //--备注信息--
+                  .HasColumnName(nameof(Department.Remark));                //字段名称
+            entity.Ignore(d => d.Reserved);                                 //忽略字段：不创建此字段
         }
 
         /// <summary>
